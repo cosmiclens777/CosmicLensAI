a6baf0b59154f655aa521535c46ef1cbe3e5052c
cosmiclens777 <cosmiclens677@gmail.com>
2026-07-06 19:15:03 +0545

    Update notes.routes.js

--- routes/notes.routes.js
+++ routes/notes.routes.js
@@ -1,7 +1,12 @@
+
+import { Router } from "express";
+
 import { generatePDF } from '../controllers/createNote.controller.js';
 import { generateQuiz } from '../controllers/createQuiz.Controller.js';
 
-const router = express.Router();
+const router = Router();
 
 router.post('/generate-pdf', generatePDF);
 router.post('/generate-quiz', generateQuiz);
+
+export default router;
