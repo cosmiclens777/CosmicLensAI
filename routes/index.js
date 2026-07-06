7a4fb60061bb477b7d986d6f8c9497ed3f1f5e55
cosmiclens777 <cosmiclens677@gmail.com>
2026-07-06 19:17:22 +0545

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import chartRoutes from "./chart.routes.js";
+import studentNotesRoutes from "./notes.routes.js";
 
 const router = Router();
 
 router.use("/chart", chartRoutes);
+router.use("/studentNotes",studentNotesRoutes)
 
 export default router;
